7dd9418ad78d4f7125cd10417c287dc53b835c5d
VarunaLex <lex.varuna@gmail.com>
2020-10-19 16:37:23 +0530

    Init four main migrations, models and contr

---
+++ database/migrations/2020_10_19_110028_create_marks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->string('reg_no');
+            $table->integer('grade');
+            $table->integer('term', 3);
+            $table->integer('marks', 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marks');
+    }
+}
